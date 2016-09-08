062c8918dc26e8679700e94008f91aed9f3a19c2
Jigesh Raval <jigeshraval89@gmail.com>
2016-09-09 00:12:02 +0530

    Default Index page defined in routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,7 @@
 <?php
+Flight::route('/', function () {
+    echo 'This is my simple PHP App';
+});
 Flight::route('/new/@myname/surname/@raval', ['UserController', 'getUserId']);
 Flight::route('/about', function () {
     echo 'About us Page';
