b584e3fdd2e100979c9444bc7042c92cc0551359
IT-Innovator Web Apps Services <jigeshraval89@gmail.com>
2016-09-13 22:26:12 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'DB_SERVER' => 'localhost',
-    'DB_USER' => 'initinno_jigesh',
-    'DB_PASSWORD' => 'Ravalera1#',
-    'DB' => 'initinno_myphp_app'
+    'DB_USER' => 'user',
+    'DB_PASSWORD' => 'password',
+    'DB' => 'db_name'
 ];
